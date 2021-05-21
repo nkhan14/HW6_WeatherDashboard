a80cd62847b9d2c47a7ab27dc0066106a543b90c
Nabiha Khan <nabiha@northwestern.edu>
2021-05-21 09:53:55 -0500

    consts added

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// a couple of consts that I can think of at the momement
+function initPage() {
+  const inputEl = document.getElementById("city-input");
+  const searchEl = document.getElementById("search-button");
+  const clearEl = document.getElementById("clear-history");
+  const nameEl = document.getElementById("city-name");
+  const currentPicEl = document.getElementById("current-pic");
+  const currentTempEl = document.getElementById("temperature");
+  const currentHumidityEl = document.getElementById("humidity");
+  const currentWindEl = document.getElementById("wind-speed");
+  const currentUVEl = document.getElementById("UV-index");
+  const historyEl = document.getElementById("history");
+  
