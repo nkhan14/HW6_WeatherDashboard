d706699f7fa7405b244603cc638003facabbc1b0
Nabiha Khan <nabiha@northwestern.edu>
2021-05-21 10:07:33 -0500

    API Key

--- assets/script.js
+++ assets/script.js
@@ -10,4 +10,12 @@ function initPage() {
   const currentWindEl = document.getElementById("wind-speed");
   const currentUVEl = document.getElementById("UV-index");
   const historyEl = document.getElementById("history");
+  let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+  console.log(searchHistory);
+
+  const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
+  //  When search button is clicked, read the city name typed by the user
+
   
+}
+initPage();
